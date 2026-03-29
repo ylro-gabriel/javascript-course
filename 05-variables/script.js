3a065dbb837bccb48574d7ff71a3738358dfc0e9
ylro-gabriel <ylrgbrlmllr@gmail.com>
2026-03-29 22:54:15 +0800

    Add cart project files with quantity manipulation functionality

---
+++ 05-variables/script.js
@@ -0,0 +1,28 @@
+// create a variable named variable2 then assign a value of 3
+let variable1 = 3;
+// display the value inside variable1
+console.log(variable1);
+
+// create a variable named calculation then assign 2+2 as a value
+const calculation = 2 + 2;
+// display the value of calculation in the console
+console.log(calculation);
+// display the value of calculation plus 2 in the console
+console.log(calculation + 2);
+
+let result = calculation + 2;
+console.log(result);
+
+let message = "hello";
+console.log(message);
+
+// change/update/reassign the value of variable1
+variable1 = 5;
+console.log(variable1);
+
+// reassign variable1 with itself but plus 1
+variable1 = variable1 + 1;
+console.log(variable1);
+
+// create a variable that a value cannot change
+const variable2 = 3;
